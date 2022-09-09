2c0bcb0520495030dd08a944f3f68639286c1044
Almadny <Almadny2u@gmail.com>
2022-09-09 15:09:52 -0700

    0 to 9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * n: Number starting from zero
+ *
+ * Description: A Program that prints numbers 0-9
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	n = 48;
+	while (n < 57)
+	{
+		putchar(n);
+		n++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
