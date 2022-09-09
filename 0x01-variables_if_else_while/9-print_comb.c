ac8149a128f6825ea7b1f2000de8ba1fe5ccc81a
Almadny <Almadny2u@gmail.com>
2022-09-09 16:38:48 -0700

    modified

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,21 +13,15 @@
 int main(void)
 {
 	int a;
-	int b;
 	int c;
 
 	c = 44;
 
 	for (a = 48; a <= 57; ++a)
 	{
-		for (b = 48; b <= 57; ++b)
-		{
-			putchar(a);
-			putchar(b);
-			putchar(c);
-		}
+		putchar(a);
+		putchar(c);
 	}
-
 	putchar('\n');
 	return (0);
 }
