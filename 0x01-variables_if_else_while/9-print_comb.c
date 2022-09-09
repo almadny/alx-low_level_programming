e3845ff2c99a39a555af3eed6566be5670711549
Almadny <Almadny2u@gmail.com>
2022-09-09 16:34:09 -0700

    fix

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,15 +12,15 @@
  */
 int main(void)
 {
-	char a;
-	char b;
-	char c;
+	int a;
+	int b;
+	int c;
 
 	c = 44;
 
-	for (a = '0'; a <= '9'; ++a)
+	for (a = 48; a <= 57; ++a)
 	{
-		for (b = '0'; b <= '9'; ++b)
+		for (b = 48; b <= 57; ++b)
 		{
 			putchar(a);
 			putchar(b);
