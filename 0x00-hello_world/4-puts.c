08f3244c1c85ed7afcd58b69308ea44c159d53c2
Almadny <Almadny2u@gmail.com>
2022-09-08 21:32:43 -0700

    remove errrors

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
  * main - Entry point
  *
  * Return: Always return 0 (success)
- * */
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts ("Programming is like building a multilingual puzzle\n");
 	return(0);
 }
