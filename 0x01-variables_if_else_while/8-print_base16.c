078f4405c3dd45e2fe8b6722ef0782263ee09110
Almadny <Almadny2u@gmail.com>
2022-09-09 17:11:46 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print hexadecimal number
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 15; ++a)
+	{
+		putchar(a);
+	}
+
+	putchar('\n')
+	return (0);
+}
