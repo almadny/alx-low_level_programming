1b515bd08b8af286b04eb57c32b6f1a56c17153d
Almadny <Almadny2u@gmail.com>
2022-09-10 02:45:19 -0700

    100 optional task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * a: first digit
+ * b: second digit
+ * c: comma ascii code
+ * s: space ascii code
+ *
+ * Description: The program prints all combinations of 2 digit numbers
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char a;
+	char b;
+	int c;
+	int s;
+
+	c = 44;
+	s = 32;
+
+	for (a = '0'; a < 10; ++a)
+	{
+		for (b = '0'; b < 10; ++b)
+		{
+			if ((a == b) || ((a + b) == (b + a)))
+				
+				continue;
+			
+			putchar(a);
+			putchar(b);
+			putchar(c);
+			putchar(s);
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
