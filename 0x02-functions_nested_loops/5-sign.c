6ee2edf1312e6a132dc66ad31789b30dbb2cc34e
Almadny <Almadny2u@gmail.com>
2022-09-13 13:20:19 -0700

    fix

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,15 +15,18 @@ int print_sign(int n)
 	{
 		return (1);
 		_putchar('+');
+		_putchar(n);
 	}
 	else if (n == 0)
 	{
 		return (0);
 		_putchar('0');
+		_putchar(n);
 	}
 	else
 	{
 		return (-1);
 		_putchar('-');
+		_putchar(n);
 	}
 }
