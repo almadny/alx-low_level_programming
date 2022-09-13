2e345424c7afb9f3d04f112d366ee63b39cf1d56
Almadny <Almadny2u@gmail.com>
2022-09-13 09:35:56 -0700

    fix betty and newline errors

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,19 @@
 #include "main.h"
 /**
- * printalphabet: Prints alphabets in lowercase
+ * print_alphabet: Prints alphabets in lowercase
  *
+ * &alp: character that represents the alphabets
+ *
+ * Description: A function that prints lowercase letters a to z
  * Return: Void
  */
 void print_alphabet(void)
 {
 	char alp;
+	
 	for (alp = 'a'; alp <= 'z'; alp++)
 	{
 		_putchar(alp);
+		_putchar('\n');
 	}
 }
