64e2ff38947189ee112c29d82e3852824cae95a5
Almadny <Almadny2u@gmail.com>
2022-09-13 09:31:17 -0700

    added a print_alphabet function prototype in main.h

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * printalphabet: Prints alphabets in lowercase
+ *
+ * Return: Void
+ */
+void print_alphabet(void)
+{
+	char alp;
+	for (alp = 'a'; alp <= 'z'; alp++)
+	{
+		_putchar(alp);
+	}
+}
