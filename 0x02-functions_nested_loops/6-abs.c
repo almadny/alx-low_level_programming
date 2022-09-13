7e9182b644e3ed4c62f1285dd40500bab9161a3b
Almadny <Almadny2u@gmail.com>
2022-09-13 14:34:19 -0700

    6 abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+/**
+ * _abs - Calulate the absolute value of a number
+ *
+ * @int: The number to get absolute value of
+ *
+ * Description: The function receives a number
+ * find and return its absolute value
+ *
+ * Return: The absolute value
+ */
+int _abs(int)
+{
+	int r;
+
+	r = abs(int);
+	return (r);
+}
