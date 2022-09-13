5e1d0c06034ed3ba6ae26374e54ad4ca5dd3f8f3
Almadny <Almadny2u@gmail.com>
2022-09-13 10:02:32 -0700

    fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,16 +4,16 @@
  *
  * &alp: character that represents the alphabets
  *
- * description: A function that prints lowercase letters a to z 
+ * description:A function that prints lowercase letters a to z
  * Return: Nothing
  */
 void print_alphabet(void)
 {
 	char alp;
-	
+
 	for (alp = 'a'; alp <= 'z'; alp++)
 	{
 		_putchar(alp);
-		_putchar('\n');
 	}
+	_putchar('\n')
 }
