bb463f587b5c9e6ec6c229d76182fab9259c6ce1
Almadny <Almadny2u@gmail.com>
2022-09-13 20:26:12 -0700

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_to_98 - Prints from a given number to 98
+ *
+ * @n: number to start print from
+ *
+ * Description: Prints from a given number to 98
+ *
+ * Return: Nothing
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		for (n = n; n <= 98; n++)
+		{
+			_putchar(n + ' ');
+			_putchar(',');
+		}
+	}
+	else
+	{
+		for (n = n; n >= 98; n--)
+		{
+			_putchar(n + ' ');
+			_putchar(',');
+		}
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+void print_to_98(int n);
 int add(int,int);
 void jack_bauer(void);
 int print_last_digit(int);
