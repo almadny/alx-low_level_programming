501d8b9c5be047a9026c34c5f038c3e33a58883c
Almadny <Almadny2u@gmail.com>
2022-09-13 21:08:59 -0700

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * times_table - Prints times table
+ * n: Number to multiple
+ * m: Number to multiple with
+ * Description: Prints a multiplication table
+ * Return: Nothing;
+ */
+void times_table(void)
+{
+	int n, m;
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (m = 0; m <= 9; n m++)
+		{
+			printf("%d, ", n * m);
+		}
+		printf("\n");
+	}
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+void times_table(void);
 void print_to_98(int n);
 int add(int,int);
 void jack_bauer(void);
