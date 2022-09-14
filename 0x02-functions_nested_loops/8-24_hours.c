133fbb26d899022865b226e69df8761d75831019
Almadny <Almadny2u@gmail.com>
2022-09-13 20:06:09 -0700

    fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,24 +9,18 @@
  */
 void jack_bauer(void)
 {
-	int m_s, m_n, s_s, s_n;
+	int m_s, m_n;
 
-	for (m_s = 0; m_s <= 2; m_s++)
+	for (m_s = 0; m_s <= 23; m_s++)
 	{
-		for (m_n = 0; m_n <= 3; m_n++)
+		for (m_n = 0; m_n <= 59; m_n++)
 		{
-			for (s_s = 0; s_s <= 5; s_s++)
-			{
-				for (s_n = 0; s_n <= 9; s_n++)
-				{
-					_putchar(m_s);
-					_putchar(m_n);
-					_putchar(':');
-					_putchar(s_s);
-					_putchar(s_n);
-					_putchar('\n');
-				}
-			}
+			_putchar((m_s / 10) + '0');
+			_putchar((m_s % 10) + '0');
+			_putchar(':');
+			_putchar((m_n / 10) + '0');
+			_putchar((m_n % 10) + 0);
+			_putchar('\n');
 		}
 	}
 }
