81feb9ab925a8394b12a2bb123d750daa6f152f4
Almadny <Almadny2u@gmail.com>
2022-09-13 20:36:06 -0700

    fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,20 +11,18 @@
  */
 void print_to_98(int n)
 {
-	if (n < 98)
+	if (n <= 98)
 	{
 		for (n = n; n <= 98; n++)
 		{
-			_putchar(n + ' ');
-			_putchar(',');
+			printf("%d, ", n);
 		}
 	}
 	else
 	{
 		for (n = n; n >= 98; n--)
 		{
-			_putchar(n + ' ');
-			_putchar(',');
+			printf("%d, ", n);
 		}
 	}
 }
