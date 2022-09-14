13021fbcdb26d3ed21edc28f932e4c6234abadcc
Almadny <Almadny2u@gmail.com>
2022-09-13 19:56:30 -0700

    fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,13 +11,12 @@
  */
 int print_last_digit(int n)
 {
-	if (isdigit(n))
-	{
-		int l;
+	int l;
 
-		l = n % 10;
-		_putchar(l);
-		return (l);
-	}
-	return
+	l = n % 10;
+	if (l < 0)
+		l *= -1;
+
+	_putchar(l + '0');
+	return (l);
 }
