c6e47965dd5da5f40db86ec93f9e312133c0ebd7
Almadny <Almadny2u@gmail.com>
2022-09-13 20:16:46 -0700

    add function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers
+ *
+ * @f: First operand
+ * @s: Second Operand
+ *
+ * Description: Sums f and s and return their result;
+ *
+ * Return: result of adding f and s;
+ */
+int add(int f, int s)
+{
+	return (f + s);
+}
