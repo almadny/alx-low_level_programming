a73342312b8f711e53b6e908fc95ea0a05d82f65
Almadny <Almadny2u@gmail.com>
2022-09-15 15:10:41 -0700

    Fizz Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * fizzbuzz - Prints fizzbuzz
+ *
+ * Description: Prints 1 to 100 and fizzbuzz
+ * Return : void
+ */
+void fizzbuzz(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 3 == 0)
+		{
+			_putchar('F');
+			_putchar('i');
+			_putchar('z');
+			_putchar('z');
+		}
+		else if (a % 5 == 0)
+		{
+			_putchar('B');
+			_putchar('u');
+			_putchar('z');
+			_putchar('z');
+		}
+		else if (a % 3 == 0 && a % 5 == 0)
+		{
+			_putchar('F');
+			_putchar('i');
+			_putchar('z');
+			_putchar('z')
+			_putchar('B');
+			_putchar('u');
+			_putchar('z');
+			_putchar('z');
+		}
+		else
+		{
+			_putchar(a);
+		}
+	}
+}
+
