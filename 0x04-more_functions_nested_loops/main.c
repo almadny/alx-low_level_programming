f44ab28f079b2d4dc665f494dd85669bfec55a1d
Almadny <Almadny2u@gmail.com>
2022-09-15 08:25:56 -0700

    other initial files

---
+++ 0x04-more_functions_nested_loops/main.c
@@ -0,0 +1,18 @@
+#include "main.c"
+#include <stdio.h>
+/**
+ * main - Main entry
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	if (_isupper('A'))
+	{
+		printf("Your character is upper case");
+	}
+	else
+	{
+		printf("It is not uppercase");
+	}
+}
