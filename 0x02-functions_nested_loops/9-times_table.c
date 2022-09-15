f0ca258066312326aca00a2a009e3872a0c062c0
Almadny <Almadny2u@gmail.com>
2022-09-14 21:02:23 -0700

    fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,7 +21,7 @@ void times_table(void)
 			{
 				if (p < 10)
 				{
-					_putchar(p);
+					_putchar(p + '0');
 					_putchar(',');
 					_putchar(' ');
 				}
@@ -37,7 +37,7 @@ void times_table(void)
 			{
 				if (p < 10)
 				{
-					_putchar(p);
+					_putchar(p + '0');
 				}
 				else
 				{
