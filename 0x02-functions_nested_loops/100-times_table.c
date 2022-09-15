db99bfc3f065db3c0c5a751c995a163cb362c2bc
Almadny <Almadny2u@gmail.com>
2022-09-14 21:43:35 -0700

    fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,9 +14,9 @@ void print_times_table(int n)
 
 	if (n <= 15 && n > 0)
 	{
-		for (i = 0; i <= n; i++)
+		for (i = 0; i <= 9; i++)
 		{
-			for (j = 0; j < 10; j++)
+			for (j = 0; j < n; j++)
 			{
 				if (j < 9)
 					printf("%d, ", i * j);
