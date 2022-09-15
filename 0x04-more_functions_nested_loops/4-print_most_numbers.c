4a591268821fbbc907641d202fdfc75c659fa9d9
Almadny <Almadny2u@gmail.com>
2022-09-15 13:16:26 -0700

    fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,9 +2,10 @@
 /**
  * print_most_numbers - print numbers 0 to 9 without 2 and 4
  *
+ * Description: Prints 0 to 9 without 2 and 4
  * Return:void
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int a;
 
