93fa64bf9f4931d410c0cea9fd40711dc9963b66
Almadny <Almadny2u@gmail.com>
2022-09-16 07:31:56 -0700

    7 lines

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print lines in diagonal
+ *
+ * @n: Number of lines and rows to print
+ *
+ * Description: Prints a diagonal line with a pattern
+ * Return: Nothing
+ */
+void print_diagonal(int n)
+{
+	int row, col, space;
+
+	for (row = 1; row <= n; row++)
+	{
+		for (col = 1; col <= row - 1; col++)
+		{
+			_putchar(' ');
+		}
+		for (space = 1; space <= 1; space++)
+		{
+			_putchar("\\");
+		}
+		_putchar('\n');
+	}
+}
+
