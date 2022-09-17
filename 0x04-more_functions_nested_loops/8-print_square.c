725420abddca4677c959a7d7161ef172cc007b78
Almadny <Almadny2u@gmail.com>
2022-09-16 20:39:04 -0700

    fixes

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,15 +9,19 @@ void print_square(int n)
 {
 	int a, b;
 
-	for (a = 0; a < n; a++)
+	if (n > 0)
 	{
-		if (n > 0)
+		for (a = 0; a < n; a++)
 		{
 			for (b = 0; b < n; b++)
 			{
 				_putchar('#');
 			}
+			_putchar('\n');
 		}
+	}
+	else
+	{
 		_putchar('\n');
 	}
 }
---
+++ 0x04-more_functions_nested_loops/9-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	fizzbuzz();
+
+	return (0);
+}
