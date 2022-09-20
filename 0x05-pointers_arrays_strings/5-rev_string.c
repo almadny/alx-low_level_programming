638aee9f365d1ba096830f99b5cbd82169a2690a
Almadny <Almadny2u@gmail.com>
2022-09-19 19:31:22 -0700

    initial reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * print_rev - Print a string in reverse
+ * @s: string to be reversed
+ * Description: Print a string in reverse
+ * Return: Nothing
+ */
+void print_rev(char *s)
+{
+	int i, j, k;
+	int len;
+
+	len = 0;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+
+	for (j = len - 1, k = 0; j > len / 2; j--, k++)
+	{
+		c = s[k];
+		s[k] = s[j];
+		s[j] = c;
+	}
+}
