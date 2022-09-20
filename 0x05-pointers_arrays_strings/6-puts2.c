2e3c5f1032244494bf6b22b00656e126cdff2df5
Almadny <Almadny2u@gmail.com>
2022-09-19 19:57:48 -0700

    6 puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of string
+ * @s: String to manipulate
+ *
+ * Description: Prints every other character of string
+ * Return: Nothing
+ */
+void puts2(char *s)
+{
+	int i, j;
+	int len;
+
+	len = 0;
+	while (s[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+	for (j = 0; j <= len; j + 2)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
