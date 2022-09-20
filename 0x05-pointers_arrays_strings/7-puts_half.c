cc49ba90164bde6632c395491a015b4155707ba7
Almadny <Almadny2u@gmail.com>
2022-09-19 20:27:23 -0700

    7-putshalf added

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * puts_half - prints every other character of string
+ * @str: String to manipulate
+ *
+ * Description: Prints every other character of string
+ * Return: Nothing
+ */
+void puts_half(char *str)
+{
+	int i, j;
+	int len;
+	char c;
+
+	len = 0;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+	for (j = (len - 1) / 2; j <= len - 1; j += 2)
+	{
+		c = s[j];
+		write(1, &c, 1);
+	}
+	_putchar('\n');
+}
