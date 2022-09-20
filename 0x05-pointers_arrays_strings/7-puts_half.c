ccbb6cc9ac2752f987c9f853e3fb62e473ab8d28
Almadny <Almadny2u@gmail.com>
2022-09-19 21:05:34 -0700

    fix

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -22,7 +22,7 @@ void puts_half(char *str)
 	}
 	if (len % 2 == 0)
 	{
-		for (j = len / 2; j <= len - 1; j++)
+		for (j = len / 2; j < len; j++)
 		{
 			c = str[j];
 			write(1, &c, 1);
@@ -30,7 +30,7 @@ void puts_half(char *str)
 	}
 	else
 	{
-		for (j = (len - 1) / 2; j <= len - 1; j++)
+		for (j = (len + 1) / 2; j < len; j++)
 		{
 			c = str[j];
 			write(1, &c, 1);
