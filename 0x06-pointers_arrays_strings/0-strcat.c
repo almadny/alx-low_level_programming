b1854c8bfbf0ae6d136675f5608fe6a94d8edacc
Almadny <Almadny2u@gmail.com>
2022-09-22 10:42:10 -0700

    initial commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strcat - Concatenates strings
+ *
+ * @*dest: The string to be added to
+ * @*src: The string to be added
+ *
+ * Description: Receives and add the src chars to the dest array
+ * Return: Returns a pointer to the char
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, d_len;
+
+	d_len = strlen(dest);
+	for (i = 0; *src != '\0'; src++)
+	{
+		dest[d_len] = src[i];
+	}
+	if (src[i] == '\0')
+	{
+		dest[d_len + s_len] = '\0';
+	}
+	return dest;
+}	
