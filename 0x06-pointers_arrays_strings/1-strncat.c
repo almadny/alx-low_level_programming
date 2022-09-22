194360551dea0ad723e6d499deab688bfc4d14f4
Almadny <Almadny2u@gmail.com>
2022-09-22 14:33:02 -0700

    fix

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <string.h>
 /**
- * *_strncpy - Concatenates strings
+ * *_strncat - Concatenates strings
  *
  * @dest: The string to be added to
  * @src: The string to be added
@@ -9,7 +9,7 @@
  * Description: Receives and add the src chars to the dest array
  * Return: Returns a pointer to the char
  */
-char *_strncpy(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
 	int i, d_len;
 
