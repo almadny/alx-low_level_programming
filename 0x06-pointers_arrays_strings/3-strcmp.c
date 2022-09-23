7cb592c0e51b2dcc550f04c904ed9db9800d2628
Almadny <Almadny2u@gmail.com>
2022-09-22 19:30:41 -0700

    fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,11 +1,10 @@
 #include "main.h"
 #include <string.h>
 /**
- * strcmp - Compares strings
+ * _strcmp - Compares strings
  *
  * @s1: The string to be added to
  * @s2: The string to be added
- * 
  * Description: Receives and add the src chars to the dest array
  * Return: Returns a pointer to the char
  */
@@ -16,6 +15,5 @@ int _strcmp(char *s1, char *s2)
 		s1++;
 		s2++;
 	}
-
 	return (*s1 - *s2);
 }
