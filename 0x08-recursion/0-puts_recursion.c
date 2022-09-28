896bf2e95540b492b18e386e95900f55e8b86c14
Almadny <Almadny2u@gmail.com>
2022-09-28 04:33:53 -0700

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _puts_recursion - Prints strings using recursion
+ * @s: String to print
+ * Description: Uses putchar and recursion to print character one by one
+ *
+ * Return: Nothing
+ */
+void _puts_recursion(char *s)
+{
+	if (strlen(s) > 0)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+		_putchar('\n');
+	}
+}
