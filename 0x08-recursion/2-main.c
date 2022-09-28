0e94625504391a67d5bc23b0e612a85cdf5e61ee
Almadny <Almadny2u@gmail.com>
2022-09-28 11:13:15 -0700

    strlen function using recursion

---
+++ 0x08-recursion/2-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ **/
+
+int main(void)
+{
+	int p = _strlen_recursion("Puts with recursion");
+	printf("%d",p);
+	return (0);
+}
