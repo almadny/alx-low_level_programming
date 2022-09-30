936c28828343b272a9ff00e72f106c87e7a39218
Almadny <Almadny2u@gmail.com>
2022-09-30 15:40:18 -0700

    4 add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,17 +10,26 @@
  */
 int main(int argc, char *argv[])
 {
-	int p;
+	int i, sum = 0;
 
 	if (argc > 2)
 	{
-		p = atoi(argv[1]) * atoi(argv[2]);
-		printf("%d\n", p);
+		for (i = 1; i < argc; i++)
+		{
+			if (atoi(argv[i]))
+				sum += atoi(argv[i]);
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", sum);
+
 	}
 	else
 	{
-		printf("Error\n");
-		return (1);
+		printf("%d\n", 0);
 	}
 	return (0);
 }
