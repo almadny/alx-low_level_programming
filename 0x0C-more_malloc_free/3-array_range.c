24560e52384bb24eaf98570c201e33e9040d446e
Almadny <Almadny2u@gmail.com>
2022-10-07 08:03:43 -0700

    range array

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * array_range - allocates memory dynamically
+ * @min: starting point
+ * @max: ending point
+ *
+ * Return: pointer of result or NULL
+ */
+int *array_range(int min, int max)
+{
+	int *p;
+	int len;
+	unsigned int i;
+
+	len = 0;
+	if (min > max)
+		return (NULL);
+
+	while (min++ < max)
+		len++;
+
+	p = malloc(sizeof(int) * len);
+
+	if (p)
+		for (i = 0; i < max; i++)
+			p[i] = min + i;
+		return (p);
+	return (NULL);
+}
