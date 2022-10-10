27231e7fbde6346c3b9649bef90751ecbdf9592e
Almadny <Almadny2u@gmail.com>
2022-10-10 11:27:49 -0700

    fix documentation

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - dog struct
+ * @name: first member
+ * @age: second member
+ * @owner: thrid member
+ *
+ * description: a structure that defines a dog
+ */
 struct dog
 {
 	char *name;
