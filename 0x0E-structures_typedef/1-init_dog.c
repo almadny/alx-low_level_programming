54344b205acff31091f97f868b156b64b7ac0a77
Almadny <Almadny2u@gmail.com>
2022-10-10 11:46:12 -0700

    fix

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -5,9 +5,6 @@
  * @name: dog name
  * @age: dog age
  * @owner: dog owner
- *
- * Return: Nothing
- * Description: Initializes a dog struxt with the argument
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
