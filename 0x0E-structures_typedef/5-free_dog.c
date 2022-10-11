375154b55a37ca9a9225e49d4701c553d2831e6c
Almadny <Almadny2u@gmail.com>
2022-10-10 19:16:38 -0700

    free dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ * free_dog - initializes a dog
+ * @d: pointer variable
+ *
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+		free(d);
+}
