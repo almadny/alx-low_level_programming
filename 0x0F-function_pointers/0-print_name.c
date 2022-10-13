5fb7cfe65607b1b4bbe41664591f03d0768e6c5e
Almadny <Almadny2u@gmail.com>
2022-10-12 20:06:48 -0700

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * print_name - prints ones name
+ * @name: Named to be printed
+ *
+ * Return: Nothing
+ */
+
+void print_name(char *s)
+{
+	printf("Hello, my name is %s\n", s);
+}
+
+/**
+ * p_name - calls name to print the name sent
+ * @name: Name to be printed
+ * @(*f)(char *): points to a function with a one arg.
+ *
+ * Return: Nothing
+ */
+
+void p_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
